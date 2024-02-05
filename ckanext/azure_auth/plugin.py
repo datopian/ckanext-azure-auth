c8e3f14f17c76fa06a7e72919acb79ded90db746
Michael Polidori <contact@michaelpolidori.com>
2024-02-05 11:46:44 -0500

    Fix authentication for normal login

--- ckanext/azure_auth/plugin.py
+++ ckanext/azure_auth/plugin.py
@@ -163,6 +163,29 @@ class AzureAuthPlugin(plugins.SingletonPlugin):
         if user:
             g.user = user
 
+    def authenticate(self, identity):
+        from ckan.model import User
+        from ckan.lib import signals
+
+        if not ('login' in identity and 'password' in identity):
+            return None
+
+        login = identity['login']
+        user_obj = User.by_name(login)
+        if not user_obj:
+            user_obj = User.by_email(login)
+
+        if user_obj is None:
+            log.debug('Login failed - username or email %r not found', login)
+        elif not user_obj.is_active:
+            log.debug('Login as %r failed - user isn\'t active', login)
+        elif not user_obj.validate_password(identity['password']):
+            log.debug('Login as %r failed - password not valid', login)
+        else:
+            return user_obj
+        signals.failed_login.send(login)
+        return None
+
     def login(self):
         pass
 
